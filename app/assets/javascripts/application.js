a028e24ed5898c192d7271e48ce785f94894ea8f
john <john@wordie.org>
2011-06-19 01:55:29 -0700

    added modernizr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
-// FIXME: Tell people that this is a manifest file, real code should go into discrete files
-// FIXME: Tell people how Sprockets and CoffeeScript works
-//
-//= require modernizr-1.7.min.js
+// This is a custom build of modernizr-2.0.4, with almost all the options included.
+// Once your app is complete and you know what you need, you may want to regenerate it
+// with only those options, to make it smaller: http://www.modernizr.com/download/
+//= require modernizr.custom.08152
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
